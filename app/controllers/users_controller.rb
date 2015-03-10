113820a7a1204f8c1e80f74266ec415cfd3b69c1
Reformant <keanon.okeefe@sendgrid.com>
2015-03-09 19:52:59 -0600

    added flash success and error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,15 +10,15 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 			if @user.save
-			# Handle a successful save.
+				redirect_to @user
+				flash[:success] = "Welcome to the Sample App!"
 			else
-			render 'new'
+				render 'new'
 			end
 	end
 
 	private
 		def user_params
-		params.require(:user).permit(:name, :email, :password,
-		                   :password_confirmation)
+			params.require(:user).permit(:name, :email, :password, :password_confirmation)
 		end
 end
